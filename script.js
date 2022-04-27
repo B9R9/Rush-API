f539b6ae2d83c8f9b911d9706874e38bc4ed55a2
B9R9 <baptiste.riffard@gmail.com>
2022-04-28 00:38:06 +0300

    Starting

---
+++ script.js
@@ -0,0 +1,15 @@
+const title = document.getElementById('welcome')
+const message_1 = "Welcome to our rush API project"
+
+function typewriter(word, index){
+    if(index < word.length) {
+        setTimeout(() => {
+            title.innerHTML += `<span>${word[index]}</span>`
+            typewriter(message_1, index +1)
+        }, 200)
+    }
+}
+setTimeout(() => {
+    typewriter(message_1, 0)
+}, 500);
+
